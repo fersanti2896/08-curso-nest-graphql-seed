ebe63eea7520537073ff41b1049d0e684554164d
Fernando Nicolás <fersanti2896@gmail.com>
2023-10-10 02:36:11 -0600

    Creando protección de seed en producción y borrado de usuarios e items.

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,8 +1,19 @@
+import { ConfigModule } from '@nestjs/config';
+import { ItemsModule } from '../items/items.module';
 import { Module } from '@nestjs/common';
-import { SeedService } from './seed.service';
 import { SeedResolver } from './seed.resolver';
+import { SeedService } from './seed.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  providers: [SeedResolver, SeedService],
+  providers: [
+    SeedResolver, 
+    SeedService
+  ],
+  imports: [
+    ConfigModule,
+    ItemsModule,
+    UsersModule
+  ]
 })
 export class SeedModule {}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Item } from '../items/entities/item.entity';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class SeedService {
-    constructor() {}
+    private isProd: boolean;
+
+    constructor(
+        private readonly configService: ConfigService,
+
+        @InjectRepository( Item )
+        private readonly itemsRepository: Repository<Item>,
+
+        @InjectRepository( User )
+        private readonly usersRepository: Repository<User>,
+    ) {
+        this.isProd = configService.get('STATE') === 'prod';
+    }
 
     async executeSeed() {
-        /* Limpiando la base de datos - Borrar todo */
+        if( this.isProd ) {
+            throw new UnauthorizedException('No se puede ejecutar el SEED en producción.');
+        }
 
+        /* Limpiando la base de datos - Borrar todo */
+        await this.deleteDatabase();
 
         /* Crear usuarios */
 
 
         /* Crear items */
-        
+
 
         return true;
+    }
+
+    async deleteDatabase() {
+        /* Se borra items */
+        await this.itemsRepository.createQueryBuilder().delete().where({}).execute();
+
+        /* Se borra usuarios */
+        await this.usersRepository.createQueryBuilder().delete().where({}).execute();
     }
 }
